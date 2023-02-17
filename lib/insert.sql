776898a6eef8421aca7c1c204e8dac4550b0e07b
emmanuelmutisya254@gmail.com <EmmanuelTheWebDeveloper>
2023-02-17 13:59:22 +0300

    Lab Completed

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 3, 'M', 'Black', 'Aggressive', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 7, 'F', 'Blonde', 'Calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 4, 'M', 'Brown', 'Assertive', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 2, 'F', 'White', 'Docile', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 9, 'M', 'Black', 'Grumpy', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 6, 'F', 'Blonde', 'Energetic', 1);
+
+INSERT INTO bears (age, sex, color, temperament, alive)
+VALUES (1, 'F', 'Brown', 'Calm', 1);
